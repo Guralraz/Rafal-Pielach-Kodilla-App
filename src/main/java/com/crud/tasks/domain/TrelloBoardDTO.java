365c628e8773c6b578f507a69e1bde8274281290
Guralraz <87212802+Guralraz@users.noreply.github.com>
2022-04-13 22:08:57 +0200

    22.2 Arranging the code task

---
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDTO.java
@@ -0,0 +1,14 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+public class TrelloBoardDTO {
+
+    private String name;
+    private String id;
+
+}
